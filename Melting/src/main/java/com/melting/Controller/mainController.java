882d2f96cac5643860e648ac0e23052334604faf
jijiji1299 <wl3330384@naver.com>
2023-05-10 04:19:56 +0900

    First Test Commit

---
+++ Melting/src/main/java/com/melting/Controller/mainController.java
@@ -0,0 +1,14 @@
+package com.melting.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainController {
+	
+	@GetMapping({"/", ""})
+		public String main() {
+		return "main";
+	}
+
+}
---
+++ Melting/src/test/java/com/melting/MeltingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeltingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
