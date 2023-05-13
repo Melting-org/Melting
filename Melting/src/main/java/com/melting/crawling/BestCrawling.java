160380034ee25400379657564383754a157cde79
jijiji1299 <wl3330384@naver.com>
2023-05-13 23:49:52 +0900

    유머 사이트 크롤링 수정

--- Melting/src/main/java/com/melting/crawling/BestCrawling.java
+++ Melting/src/main/java/com/melting/crawling/BestCrawling.java
@@ -1,24 +1,21 @@
 package com.melting.crawling;
 
-import java.io.IOException;
-import java.net.SocketException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BestCrawling {
 
-	public static void main(String[] args) throws SocketException {
+	public static void main(String[] args) throws Exception {
 		
 		Path path = Paths.get("C:/SetupFiles/chromedriver/chromedriver.exe");
 		String url = "https://www.dcinside.com/";
@@ -34,138 +31,90 @@ public class BestCrawling {
 		
 		WebDriver driver = new ChromeDriver(chromeOptions);
 		
-		// WebDriver 가 로드될때까지 5초 기다림
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+		// WebDriver 가 로드될때까지 10초 기다림
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		
 		// 웹 페이지 띄우기
-		driver.get(url);
-
-		try {
-			driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li[1]/a/div[1]/img")).click();
-			String title1 = driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li[1]/a/div[2]/p")).getText();
-			String author1= driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[1]/span[1]/em")).getText();
-			driver.navigate().back();
-
-			driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li[2]/a/div[1]/img")).click();
-			String title2 = driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/h3/span[2]")).getText();
-			String author2= driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[1]/span[1]/em")).getText();
-			driver.navigate().back();
-			
-			driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li[3]/a/div[1]/img")).click();
-			String title3 = driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/h3/span[2]")).getText();
-			String author3= driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[1]/span[1]/em")).getText();
-			driver.navigate().back();
-			
-			driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li[4]/a/div[1]/img")).click();
-			String title4 = driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/h3/span[2]")).getText();
-			String author4= driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[1]/span[1]/em")).getText();
-			driver.navigate().back();
-			
-			driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li[5]/a/div[1]/img")).click();
-			String title5 = driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/h3/span[2]")).getText();
-			String author5= driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[1]/span[1]/em")).getText();
-			driver.navigate().back();
-			
-			driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li[6]/a/div[1]/img")).click();
-			String title6 = driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/h3/span[2]")).getText();
-			String author6= driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[1]/span[1]/em")).getText();
-			driver.navigate().back();
-			
-			
-			
-			System.out.println("================================================");
-//			System.out.println(title);
-			System.out.println(title1);
-			System.out.println(author1);
-			
-			System.out.println(title2);
-			System.out.println(author2);
-			
-			System.out.println(title3);
-			System.out.println(author3);
-			
-			System.out.println(title4);
-			System.out.println(author4);
-			
-			System.out.println(title5);
-			System.out.println(author5);
-			
-			System.out.println(title6);
-			System.out.println(author6);
-	
-			System.out.println("================================================");
-			
-		} catch (NoSuchElementException e) {
-			System.out.println("ㅋㅋ");
-			driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li[1]/a/div[1]/img")).click();
-			String title1 = driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li[1]/a/div[2]/p")).getText();
-			String author1= driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[1]/span[1]/em")).getText();
-			driver.navigate().back();
-
-			driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li[2]/a/div[1]/img")).click();
-			String title2 = driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/h3/span[2]")).getText();
-			String author2= driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[1]/span[1]/em")).getText();
-			driver.navigate().back();
-			
-			driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li[3]/a/div[1]/img")).click();
-			String title3 = driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/h3/span[2]")).getText();
-			String author3= driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[1]/span[1]/em")).getText();
-			driver.navigate().back();
-			
-			driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li[4]/a/div[1]/img")).click();
-			String title4 = driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/h3/span[2]")).getText();
-			String author4= driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[1]/span[1]/em")).getText();
-			driver.navigate().back();
-			
-			driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li[5]/a/div[1]/img")).click();
-			String title5 = driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/h3/span[2]")).getText();
-			String author5= driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[1]/span[1]/em")).getText();
-			driver.navigate().back();
-			
-			driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li[6]/a/div[1]/img")).click();
-			String title6 = driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/h3/span[2]")).getText();
-			String author6= driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[1]/span[1]/em")).getText();
-			driver.navigate().back();
-			
-			
-			
-			System.out.println("================================================");
-//				System.out.println(title);
-			System.out.println(title1);
-			System.out.println(author1);
-			
-			System.out.println(title2);
-			System.out.println(author2);
-			
-			System.out.println(title3);
-			System.out.println(author3);
-			
-			System.out.println(title4);
-			System.out.println(author4);
-			
-			System.out.println(title5);
-			System.out.println(author5);
-			
-			System.out.println(title6);
-			System.out.println(author6);
-
-			System.out.println("================================================");
-			
-			
-		}	
-			
+		driver.get(url);	
+		Thread.sleep(1000);
 		
-//		NoSuchElementException
+
+		String[] title =new String[3];	//글제목
+		String[] writer =new String[3];	//작성자
+		String[] regDate =new String[3];	//글 등록일
+		String[] views =new String[3];	//조회수
+		String[] like =new String[3];	//추천수
+		String[] commentC =new String[3];	//댓글수
+//		String[] content =new String[3];	//글내용
+//		List<String> contentList = new ArrayList<String>();
+//		List<WebElement> content = null;
+//		int j;
+		
+		
+
+		// 크롤링
+		for(int i=1; i<=3; i++) {			
+			
+			driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li["+i+"]/a/div[2]/p")).click();
+			Thread.sleep(1000);
+			title[i-1] = driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/h3/span[2]")).getText();
+			writer[i-1] = driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[1]/span[1]/em")).getText();
+			regDate[i-1] = driver.findElement(By.className("gall_date")).getText();
+			views[i-1] = driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[2]/span[1]")).getText();
+			like[i-1] = driver.findElement(By.className("gall_reply_num")).getText();
+			commentC[i-1] = driver.findElement(By.xpath("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[2]/span[3]/a")).getText();
+//			content = driver.findElements(By.className("write_div")).get(j);
+			
+			
+			driver.navigate().back();
+			Thread.sleep(1000);
+			
+//			keyword_list = []
+//					
+//			keywords = bs.find_all('li', attrs={'class':'tag'})
+//	                for key in keywords:
+//	                    keyword_list.append(key.get_text())
+			
+		}
+		
+	
+
+		System.out.println("================================================");
+		
+		for (int i=0; i<3; i++) {
+			System.out.println(title[i]);
+			System.out.println(writer[i]);
+			System.out.println(regDate[i]);
+			System.out.println(views[i]);
+			System.out.println(like[i]);
+			System.out.println(commentC[i]);
+//			System.out.println(content[i]);
+//			System.out.println(content);
+		}
+//		System.out.println("dcArray"+dcArray);
+//		System.out.println(dcArray[0]);
+
+
+		System.out.println("================================================");
+			
+			
+//		} catch (SocketException e) {
+//			e.printStackTrace();
+//		}
+		
+		
+		
+
+		
 //			title.append(title1);
 //		}
         
-        
-		//*[@id="bottom_listwrap"]/section[1]/article/div[1]/table/tbody/tr[4]/td[2]/a[1]
-		//*[@id="bottom_listwrap"]/section[1]/article/div[1]/table/tbody/tr[5]/td[2]/a[1]
+
 
 		
 	
-		driver.quit();
+//		driver.quit();	//안없앨 시 SocketException: Connection reset 발생
+//		driver.close();
 		
 
 	}
--- Melting/src/main/java/com/melting/crawling/bestCrawlingTest.java
+++ Melting/src/main/java/com/melting/crawling/bestCrawlingTest.java
@@ -1,25 +1,54 @@
 package com.melting.crawling;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class bestCrawlingTest {
 
 	public static void main(String[] args) {
+		Path path = Paths.get("C:/SetupFiles/chromedriver/chromedriver.exe");
 		String url = "https://www.dcinside.com/";
 		Document doc = null;
+		
+		// chrome driver 세팅
+		System.setProperty("webdriver.chrome.driver", path.toString());
+		
+		// 브라우저 선택
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--remote-allow-origins=*");
+		
+		WebDriver driver = new ChromeDriver(chromeOptions);
+		
+		// WebDriver 가 로드될때까지 5초 기다림
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		
+		driver.get(url);
+		driver.findElement(By.xpath("//*[@id=\"container\"]/div/section[1]/article[1]/div[3]/ul[1]/li[1]/a/div[2]/p")).click();
+		driver.navigate().back();
+		
 		
 		try {
 			doc = Jsoup.connect(url).get();
 			System.out.println(doc.toString());
 			
-
+			
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		driver.navigate().back();
+		driver.quit();
 		
 		
 		
