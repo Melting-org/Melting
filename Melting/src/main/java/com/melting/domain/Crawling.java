56c6bc1fb0ff436a2d212899abc7623abfb5d072
jijiji1299 <wl3330384@naver.com>
2023-05-29 23:53:10 +0900

    crawling controller, service 생성

---
+++ Melting/src/main/java/com/melting/domain/Crawling.java
@@ -0,0 +1,30 @@
+package com.melting.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter @Getter
+@ToString
+@Builder
+
+public class Crawling {
+	
+	private String title;
+//	private String writer;
+//	private String kind;
+//	private String regdate;
+//	private String link;
+//	private int replycnt;
+//	private int viewscnt;
+//	private int likecnt;
+	
+
+}
